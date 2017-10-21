b1c236432153d46001a52d39cc6062228cb4c33c
clovis-ai <clovis.onplag@gmail.com>
2017-10-21 23:26:53 +0200

    Added support for updates of members joining

---
+++ src/bot/messages/NewMembers.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bot.messages;
+
+import bot.User;
+import minimaljson.JsonArray;
+import minimaljson.JsonObject;
+import minimaljson.JsonValue;
+
+/**
+ *
+ * @author CLOVIS
+ */
+class NewMembers extends Message {
+
+    /** The users who just joined the chat. The bot may be one of these members. */
+    public final User[] USERS;
+    
+    /**
+     * Created when users join the chat.
+     * @param json data from the Telegram servers
+     */
+    public NewMembers(JsonObject json) {
+        super(json);
+        JsonArray ja = json.get("new_chat_members").asArray();
+        USERS = new User[ja.values().size()];
+        for(int i = 0; i < USERS.length; i++){
+            USERS[i] = new User(ja.get(i).asObject());
+        }
+    }
+    
+}
