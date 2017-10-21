82a246574ceda82b659bab7a2dbf3097dbf6153f
clovis-ai <clovis.onplag@gmail.com>
2017-10-22 00:16:36 +0200

    Added PhotoSize support

---
+++ src/bot/messages/PhotoSize.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bot.messages;
+
+import bot.MandatoryFieldOmittedException;
+import minimaljson.JsonObject;
+
+/**
+ *
+ * @author CLOVIS
+ */
+public class PhotoSize {
+    
+    /** Unique identifier for this file. */
+    public final String ID;
+    
+    /** Photo width. */
+    public final int WIDTH;
+    
+    /** Photo height. */
+    public final int HEIGHT;
+    
+    /** (Optional)<br>File size. */
+    public final int SIZE;
+    
+    /**
+     * 
+     * @param json data from the Telegram servers
+     * @exception MandatoryFieldOmittedException if mandatory fields are omitted
+     */
+    public PhotoSize(JsonObject json) throws MandatoryFieldOmittedException {
+        ID = json.getString("file_id", null);
+        WIDTH = json.getInt("width", 0);
+        HEIGHT = json.getInt("height", 0);
+        SIZE = json.getInt("file_size", 0);
+        
+        if(ID == null || WIDTH == 0 || HEIGHT == 0)
+            throw new MandatoryFieldOmittedException("file_id, width or height was omitted.", json);
+    }
+    
+}
