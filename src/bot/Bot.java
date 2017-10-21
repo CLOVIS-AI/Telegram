56124a0119cee9941f79261b5bfd8233ce36aeff
clovis-ai <clovis.onplag@gmail.com>
2017-10-21 22:15:31 +0200

    Added tutorial on how to create a bot, added automatical update gathering with a thread, and renamed FIRST_NAME in NAME

--- src/bot/Bot.java
+++ src/bot/Bot.java
@@ -5,7 +5,6 @@
  */
 package bot;
 
-import bot.messages.Message;
 import bot.updates.Update;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -16,50 +15,126 @@ import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.HttpURLConnection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import minimaljson.Json;
 import minimaljson.JsonArray;
 import minimaljson.JsonObject;
 import minimaljson.JsonValue;
 
 /**
- *
+ * Class that defines a bot.<br><br>
+ * <b>Creating your own bot</b><br>
+ * To create your bot, you just have to follow these simple steps.<br>
+ * <ol>
+ *   <li>Getting your token from Telegram :
+ *     <ol>
+ *       <li>Open Telegram, and go talk with <a href="t.me/BotFather">@BotFather</a>.</li>
+ *       <li>Do <code>/newbot</code> to officially create your bot.
+ *           BotFather will ask you for a name and a username.
+ *       </li>
+ *       <li>Copy the token BotFather gives you, this is what you are going to use to identity your bot.<br>
+ *           It should look like <code>110201543:AAHdqTcvCH1vGWJxfSeofSAs0K5PALDsaw</code>.
+ *       </li>
+ *     </ol>
+ *   </li>
+ *   <li>Create a new Java project,</li>
+ *   <li>In the <code>main</code> method, create your bot as such (this is only an example) :<br>
+ *     <pre>
+ * Bot myBot = new Bot(PUT_YOUR_TOKEN_HERE){
+ *     public void onUpdate(Update u){
+ *         // What the bot should do when it recieves an update (new messages, edited messages, queries, ...)
+ *     }
+ * };
+ *     </pre>
+ *   </li>
+ *   <li>That's it ! You can run your code, the bot is working ! You just have to 
+ *       fill the {@link #onUpdate(bot.updates.Update) onUpdate()} method now.<br>
+ *       You can find more informations on the different kinds of updates {@link Update here}.
+ *   </li>
+ * </ol>
+ * 
  * @author CLOVIS
  */
-public class Bot {
+public abstract class Bot implements Runnable {
     
     /** Token as provided by BotFather. */
     private final String TOKEN;
     /** User ID of this bot. */
     public final long ID;
-    /** First name of this bot. */
-    public final String FIRST_NAME;
+    /** Name of this bot. */
+    public final String NAME;
     /** Username of this bot. */
     public final String USERNAME;
     
+    /** Updater. */
+    private final Thread updater;
+    
+    /** Time between two updates. */
+    private long updateTimeout;
+    
     /**
-     * Creates a bot and connects it to the Telegram servers.
+     * Creates a bot and connects it to the Telegram servers, and prints to the
+     * standard output the identity of the bot.<br>
+     * By default, the bot will start a thread to update itself every 5 seconds.
+     * To disable the thread, use {@link #stop() stop()} ; to set the update
+     * frequency, see {@link}
      * @param token token provided by BotFather
      */
     public Bot(String token){
+        updater = new Thread(this);
         TOKEN = token;
         JsonObject me = http("getMe").asObject().get("result").asObject();
         ID = me.getLong("id", 0);
-        FIRST_NAME = me.getString("first_name", null);
+        NAME = me.getString("first_name", null);
         USERNAME = me.getString("username", null);
         
-        if(ID == 0 || FIRST_NAME == null || USERNAME == null)
+        if(ID == 0 || NAME == null || USERNAME == null)
             throw new IllegalArgumentException("This bot has issues.");
         
-        System.out.println("Hello, I am " + FIRST_NAME + ".\n    Username : @"
+        System.out.println("Hello, I am " + NAME + ".\n    Username : @"
                 + USERNAME + "\n    ID : " + ID);
     }
     
-    public void onUpdate(Update update){}
+    /**
+     * Sets the frequency of the automatical updates.
+     * @param time time between each update, in milliseconds ; time must be greater
+     *              or equal to 100 ms.
+     * @exception IllegalArgumentException if time is lesser than 100.
+     */
+    public void setAutoUpdateFrequency(long time) throws IllegalArgumentException {
+        if(time >= 100){
+            updateTimeout = time;
+        }else{
+            throw new IllegalArgumentException("The argument 'time' should be greater or equal to 100 : " + time);
+        }
+    }
     
-    public final void update(){
+    /**
+     * This method is called whenever the bot recieves a new {@link Update update}.
+     * Override this method to act when this happens.
+     * @param update an update from the Telegram servers
+     */
+    public abstract void onUpdate(Update update);
+    
+    /**
+     * Connects to the Telegram server to get new updates, then calls
+     * {@link #onUpdate(bot.updates.Update) onUpdate()} on every update.
+     */
+    public final synchronized void update(){
         JsonArray updates = http("getUpdates").asArray();
         for(JsonValue value : updates){
             onUpdate(Update.newUpdate((JsonObject)value));
+        }
+    }
+    
+    @Override
+    public synchronized final void run() {
+        update();
+        try {
+            this.wait(updateTimeout);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Bot.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
@@ -113,4 +188,12 @@ public class Bot {
         return http(method, Json.parse("{\"status\":null}").asObject());
     }
     
+    /**
+     * Calls {@link Thread#interrupt()} on the bot's thread. This makes the bot unable
+     * to get automatic updates. The bot is still able to work properly though,
+     * if you call {@link #update() update()} manually.
+     */
+    public final void stop(){
+        updater.interrupt();
+    }
 }
